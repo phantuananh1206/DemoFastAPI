e871b6d6690a040a2f5fe4fbd7d47ce5dcdd0db2
phantuananh1206 <phantuananhltt@gmail.com>
2024-05-24 17:23:33 +0700

    FAPI-3 migration db using alembic

--- src/main.py
+++ src/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from . import models, database
 
-from . import models
-from .database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
-
-#Dependency
-def get_db():
-    db = SessionLocal()
-    try : 
-        yield db
-    finally:
-        db.close()
